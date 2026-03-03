7ef0f82f983969ce2d785803d471b62c2dfd72af
SmashcardsService <levijder@gmail.com>
2026-03-03 15:37:37 -0600

    Add files via upload

---
+++ triviaData.js
@@ -0,0 +1,54 @@
+const trivia = {
+  "Who founded this website?": "Levi Der",
+
+  "Which quizzer is technically related to their former coach Corbin Phillips and SBQ Director Angie Phillips?": "Gentry Bear",
+
+  "Who replied 'Yes XP' when asked for trivia?": "Megan Sortore",
+
+  "Who jokingly called themselves the worst quizzer on Dead BC?": "Kaylin Kastman",
+
+  "Who co-invented the 'Funky Pickle' with Eden Shepp?": "Annika Kampe",
+
+  "Which quizzer has been to France and has never played Minecraft?": "Henry Jones",
+
+  "Which quizzer once fell asleep while quoting on the phone?": "Eden Shepp",
+
+  "Who answers questions in goofy voices?": "Rannie Garrett",
+
+  "Who got Orlyn to sign their shirt at Nationals?": "Nathaniel Der",
+
+  "Who is known as 'The GOAT'?": "Orlyn Marks",
+
+  "Which quizzer was submitted as a trivia answer by Toby Garrett?": "Ryder Iantorno",
+
+  "Who has made the most championships at Nationals without winning one?": "Peter Gossell",
+
+  "Who rides dirt bikes?": "Addie Jansen",
+
+  "Which quizzer submitted their own name as trivia?": "Charis Button",
+
+  "Who hates mismatched socks in every situation except quizzing — where they insist on wearing them?": "Micah DeCaro",
+
+  "Who says they quiz best while bouncing a pillow off a wall?": "Gentry Bear",
+
+  "Who is considered the most strict Quizmaster?": "Kim Cooper",
+
+  "Who struggles to give a straight question-and-answer?": "Aryll Brown",
+
+  "Who almost killed themself by trying to climb 30 feet into a tree one-handed while holding a quiz book and water bottle?": "Lizzie Roggow",
+
+  "Who quizzes without an appendix?": "Asa (T) Serig",
+
+  "Who once had a book signing for her book at a quiz event?": "Jenny Gossell",
+
+  "Which quizzer simply submitted their own first name as trivia?": "Liam Mullins",
+
+  'Which quizzer randomly answered "Hunter" as their trivia question?': "Lorenzo Mullins",
+
+  "Who said they don't know anyone from MWTFC who does this?": "Oliver Lohrenz",
+  
+  
+  "Who said they eat knees": "Stanley Chingus ",
+  
+  "Who had the iconic “in my opinion” challenge in their rookie year that their ministry still makes fun of them for?":"Ryder Iantorno",
+};
